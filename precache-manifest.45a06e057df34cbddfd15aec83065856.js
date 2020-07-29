5692c0b48102bcd552f4656e4f38d0601f20f866
vishnu <vishnuvijayakumar125@gmail.com>
2020-07-29 23:11:53 +1000

    Updates

---
+++ precache-manifest.45a06e057df34cbddfd15aec83065856.js
@@ -0,0 +1,106 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ef42e09a45304c60be0a01bfdbba4fa8",
+    "url": "/Rhinovation/index.html"
+  },
+  {
+    "revision": "00230be80592bc8c5f86",
+    "url": "/Rhinovation/static/css/main.56ea49dd.chunk.css"
+  },
+  {
+    "revision": "5c72ebd3863960b8e5c1",
+    "url": "/Rhinovation/static/js/2.c74a7643.chunk.js"
+  },
+  {
+    "revision": "cf65305d934c66b1c53bc252e95eb343",
+    "url": "/Rhinovation/static/js/2.c74a7643.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "00230be80592bc8c5f86",
+    "url": "/Rhinovation/static/js/main.1be2e9cc.chunk.js"
+  },
+  {
+    "revision": "9e4a5fb1bd6a9c0c2ba2418c86e49769",
+    "url": "/Rhinovation/static/js/main.1be2e9cc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2d50778fa97b90e4218f",
+    "url": "/Rhinovation/static/js/runtime-main.eb3f50c7.js"
+  },
+  {
+    "revision": "50f15a1c62e8f493400f0882130f40f3",
+    "url": "/Rhinovation/static/media/bg.50f15a1c.jpg"
+  },
+  {
+    "revision": "1813f9d645d23e27edd233139d151be4",
+    "url": "/Rhinovation/static/media/bg2.1813f9d6.jpg"
+  },
+  {
+    "revision": "7f7796d84a07cfc2f47cfe5a6477f147",
+    "url": "/Rhinovation/static/media/bg3.7f7796d8.jpg"
+  },
+  {
+    "revision": "ea652077a9a9c70277df70ce1c8b4146",
+    "url": "/Rhinovation/static/media/blog5.ea652077.jpg"
+  },
+  {
+    "revision": "68d5c430da9b07976b4421717dbac1f7",
+    "url": "/Rhinovation/static/media/blog7.68d5c430.jpg"
+  },
+  {
+    "revision": "47bb87fb754b37775a7af3dffd10c490",
+    "url": "/Rhinovation/static/media/card-blog3.47bb87fb.jpg"
+  },
+  {
+    "revision": "c2cea7c5613d8a948cee95f0383f9ed4",
+    "url": "/Rhinovation/static/media/card-blog4.c2cea7c5.jpg"
+  },
+  {
+    "revision": "3122abf4a3e1067926c08dee7684522d",
+    "url": "/Rhinovation/static/media/card-profile1-square.3122abf4.jpg"
+  },
+  {
+    "revision": "788e3d36e8c4d4120daeee6e57e7e85f",
+    "url": "/Rhinovation/static/media/card-profile2-square.788e3d36.jpg"
+  },
+  {
+    "revision": "1a1649171cdecd24e7a16ea1340077df",
+    "url": "/Rhinovation/static/media/card-profile4-square.1a164917.jpg"
+  },
+  {
+    "revision": "1f1f4900f3bc166b75f8235c2b89864f",
+    "url": "/Rhinovation/static/media/card-profile6-square.1f1f4900.jpg"
+  },
+  {
+    "revision": "6857ca8c305ecd7106aa5674b086cf1c",
+    "url": "/Rhinovation/static/media/card-project1.6857ca8c.jpg"
+  },
+  {
+    "revision": "93b1cec58542e511f6798c3c38df9c7f",
+    "url": "/Rhinovation/static/media/card-project2.93b1cec5.jpg"
+  },
+  {
+    "revision": "9e15b8b0e47f3b5483816fccdce6b794",
+    "url": "/Rhinovation/static/media/card-project3.9e15b8b0.jpg"
+  },
+  {
+    "revision": "282e960fb4799f1571f8df66d8bc7f40",
+    "url": "/Rhinovation/static/media/card-project4.282e960f.jpg"
+  },
+  {
+    "revision": "516bb2deab2bbc387e6d4606f4adfef5",
+    "url": "/Rhinovation/static/media/card-project5.516bb2de.jpg"
+  },
+  {
+    "revision": "cdb05aa3618347759b9440758ee101f9",
+    "url": "/Rhinovation/static/media/card-project6.cdb05aa3.jpg"
+  },
+  {
+    "revision": "58a713c73886bac25ac6aedcbb13de5a",
+    "url": "/Rhinovation/static/media/city.58a713c7.jpg"
+  },
+  {
+    "revision": "d57fa885fa5c7002dc43e9dd61172af7",
+    "url": "/Rhinovation/static/media/office2.d57fa885.jpg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.e1a3f534b22624fdd2a2e238421ef10d.js"
+  "/Rhinovation/precache-manifest.45a06e057df34cbddfd15aec83065856.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/Rhinovation/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
